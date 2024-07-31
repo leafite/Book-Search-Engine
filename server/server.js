0d00442d6a1ef324fd2db3aec7f828d2471ec149
Chris Bautista <160420608+leafite@users.noreply.github.com>
2024-07-31 16:31:19 -0700

    Update server.js
    setting up mongo for render

--- server/server.js
+++ server/server.js
@@ -2,38 +2,57 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 require('dotenv').config();
-const db = require('./config/connection');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const { typeDefs, resolvers } = require('./schema');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context: ({ req }) => authMiddleware({ req }),
   persistedQueries: false,
 });
 
-server.start().then(() => {
-  server.applyMiddleware({ app });
+async function startServer() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
 
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+    // Start Apollo server
+    await server.start();
+    server.applyMiddleware({ app });
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/build')));
+    }
 
-  db.once('open', () => {
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+
     app.listen(PORT, () => {
-      console.log(`Now listening on http://localhost:${PORT}`);
+      console.log(`Server is running on http://localhost:${PORT}`);
       console.log(`GraphQL endpoint: http://localhost:${PORT}${server.graphqlPath}`);
     });
-  });
-});
+  } catch (error) {
+    console.error('Error starting server:', error);
+  }
+}
+
+startServer();
